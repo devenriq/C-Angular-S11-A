55509550c52bf89bc043b471198b73e458a46ab5
Enrique <octepc.97@gmail.com>
2023-01-04 13:03:37 -0500

    made the simple structure of the reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'form-app-A';
+export class AppComponent implements OnInit {
+
+  public formLogin: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.formLogin=this.formBuilder.group({
+      email:['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+  }
+
+  send():any{
+    console.log(this.formLogin.value)
+  }
 }
